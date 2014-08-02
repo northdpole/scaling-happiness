8b4843f770eba1da2a1d3d6513fb2b7fa127c589
lukebarnard <luke.barnard99@gmail.com>
2014-08-02 13:59:21 +0200

    GUI now has buttons that can do actions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,8 @@
 
 
-function Canvas(context) {
+function Canvas(canvas_element) {
 	this.screens = [];
-	this.context = context;
+	this.context = canvas_element.getContext("2d");
 
 	this.addScreen = function(s) {
 		this.screens.push(s);
@@ -21,6 +21,16 @@ function Canvas(context) {
 		this.draw(this.context);
 	}
 
+	this.mouseClick = function(event) {
+		event = {
+			x: event.offsetX / this.context.canvas.width, 
+			y: event.offsetY / this.context.canvas.height};
+
+		this.current_screen.mouseClick(event);
+	}
+	p = this;
+	canvas_element.addEventListener("click", function(event) {p.mouseClick(event);});
+
 	return this;
 }
 
@@ -33,12 +43,21 @@ function Screen() {
 		this.buttons.push(button);
 	}
 
-	this.draw = function draw(context) {
+	this.draw = function(context) {
 		this.background.draw(context);
+		for (i = 0; i < this.buttons.length; ++i) {
+			this.buttons[i].draw(context);
+		}
 	}
 
-	this.tick = function tick() {
+	this.tick = function() {
 
+	}
+
+	this.mouseClick = function(event) {
+		for (i = 0;i < this.buttons.length; ++i) {
+			this.buttons[i].mouseClick(event);
+		}
 	}
 
 	return this;
@@ -50,7 +69,7 @@ function Background(file_name) {
 	this.img.src = file_name;
 
 	this.draw = function(context) {
-		context.drawImage(this.img,0,0);
+		context.drawImage(this.img, 0, 0);
 	}
 
 	return this;
@@ -61,9 +80,35 @@ function Button(x, y, w, h) {
 	this.y = y;
 	this.w = w;
 	this.h = h;
+	this.action = function () {console.log("Button action!")}
+	this.text = "Button";
 
 	this.draw = function(context) {
-		context.fillRect(this.x, this.y, this.w * context.canvas.width, this.h * context.canvas.height);
+		scaled = {
+			x: this.x * context.canvas.width,
+			y: this.y * context.canvas.height,
+			w: this.w * context.canvas.width,
+			h: this.h * context.canvas.height
+		}
+		context.fillStyle = "#FFF";
+		context.strokeStyle = "#000";
+		context.fillRect(scaled.x, scaled.y, scaled.w, scaled.h);
+		context.strokeRect(scaled.x, scaled.y, scaled.w, scaled.h);
+
+		context.font = (scaled.h - 10) + "px Comic Sans MS";
+		context.fillStyle = "#000";
+		context.fillText(
+			this.text,
+			this.x * context.canvas.width, 
+			(this.y + this.h) * context.canvas.height - 8);
+	}
+
+	this.mouseClick = function(event) {
+		if (event.x > this.x && event.y > this.y &&
+			event.x < this.x + this.w &&
+			event.y < this.y + this.h) {
+			this.action();
+		}
 	}
 
 	return this;
@@ -71,16 +116,23 @@ function Button(x, y, w, h) {
 
 function MenuScreen() {
 	this.__proto__ = new Screen();
-	this.background = new Background("http://vividlife.me/ultimate/wp-content/uploads/2013/02/Rubber_Duck_Sea_by_whispering_hills-1.jpg");
+	this.background = new Background("/img/test/test.jpg");
 
-	console.log(this);
+	testButton = new Button(0.25, 0.25, 0.5, 0.1);
+	testButton.action = function () {
+		testButton.text = "You clicked me!";
+	}
+
+	this.addButton(testButton);
+
 	return this;
 }
 
 function load() {
-	canvas_context = document.getElementById("canvas").getContext("2d");
+	canvas_element = document.getElementById("canvas");
+	canvas_context = canvas_element.getContext("2d");
 
-	canvas = new Canvas(canvas_context);
+	canvas = new Canvas(canvas_element);
 
 	fps = 30;
 
