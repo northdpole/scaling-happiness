ccd250481165933064a1060ee74fc574fd1aaa25
Spyros Gasteratos <morfeas3000@gmail.com>
2014-08-02 18:49:29 +0200

    added add/remove functions to register and remove creeps from each wave
    Signed-off-by: Spyros Gasteratos <morfeas3000@gmail.com>

--- assets/js/wave.js
+++ assets/js/wave.js
@@ -3,6 +3,13 @@ function wave(foes,id){
 	this.id = id
 	this.start = start
 
+	function add_foe(foe){this.foes.push(foe);}
+
+	function remove_foe(id){
+		for(i=0;i,foes.length;i++)
+			if(foes[i].id == id)
+				delete foes[i];
+	}
 	/*
 	 * Makes all the creeps in the wave move towards the end
 	 */
